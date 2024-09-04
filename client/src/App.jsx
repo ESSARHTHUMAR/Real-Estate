0e26fff0120c6b685ec3bef88b4bec0346b2cc0c
ESSARHTHUMAR <essarthumar177@gmail.com>
2024-09-04 23:16:58 +0530

    Client - Sign Up Page creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,8 @@ import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import SignIn from "./pages/SignIn";
-import ContactUs from "./pages/ContactUs";
 import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
