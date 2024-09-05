f5d03e5e2d968553b3d36639df57ab0943197da0
ESSARHTHUMAR <essarthumar177@gmail.com>
2024-09-05 19:56:20 +0530

    Sign up page functionality

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.routes.js"
-import signUpRouter from "./routes/auth.route.js"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 
@@ -15,18 +15,19 @@ mongoose
 const app = express();
 
 app.use(express.json())
+
 app.listen(3000, () => {
   console.log("Hii");
 });
 
 app.use("/api/user", userRouter);
-app.use("/api/auth", signUpRouter) 
+app.use("/api/auth", authRouter) 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const errMessage = err.message || 'Internal Server error';
+  const message = err.message || 'Internal Server error';
   return res.status(statusCode).json({
     success: false,
-    statusCode: statusCode,
-    errMessage
+    statusCode,
+    message
   })
 })
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -4,6 +4,6 @@ import signUp from "../controllers/auth.controller.js";
 const router = express.Router();
 
 
-const auth = router.post("/signup", signUp)
+router.post("/signup", signUp)
 
-export default auth
+export default router
